1bb1ab3933c575aa3e35013c83bb090389b50fcc
Ming-Ho Yee <mh@mhyee.com>
2013-03-10 17:13:44 -0700

    Edit bash history settings

--- lib/history.bash
+++ lib/history.bash
@@ -3,18 +3,27 @@
 # append to bash_history if Terminal.app quits
 shopt -s histappend
 
+# Store commands on only one line
+shopt -s cmdhist
+
 # history handling
 #
-# Erase duplicates
-# Bash History
-export HISTCONTROL="ignoredups"
-export HISTCONTROL=erasedups
+# ignore duplicates and commands that start with a space
+# erase duplicates
+export HISTCONTROL=ignoreboth:erasedups
 
 # resize history size
-export HISTSIZE=5000
+export HISTFILESIZE=1000000
+export HISTSIZE=1000000
+
+# timestamp history with date and time
+export HISTTIMEFORMAT='%F %T '
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 export AUTOFEATURE=true autotest
 
 function rh {
-  history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
+  history | awk '{a[$4]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head
 }
